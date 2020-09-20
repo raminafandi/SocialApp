b75fc18a8b4e2790e52cf7daee6a59db31d2f189
Ramin Afandiyev <41924783+raminafandi@users.noreply.github.com>
2020-09-20 21:12:41 +0400

    Nothing crucial

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,26 +1,26 @@
 import * as firebase from 'firebase';
-import 'firebase/firestore'
+import 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyBBrdxfvO3khmOe67mq7FQ0wrVjOsJRfrE",
-    authDomain: "looks-f90da.firebaseapp.com",
-    databaseURL: "https://looks-f90da.firebaseio.com",
-    projectId: "looks-f90da",
-    storageBucket: "looks-f90da.appspot.com",
-    messagingSenderId: "337497640410",
-    appId: "1:337497640410:web:29b3486646ff5d644bc4e3",
-    measurementId: "G-RFV1KWFJ7C"
+  apiKey: 'AIzaSyBBrdxfvO3khmOe67mq7FQ0wrVjOsJRfrE',
+  authDomain: 'looks-f90da.firebaseapp.com',
+  databaseURL: 'https://looks-f90da.firebaseio.com',
+  projectId: 'looks-f90da',
+  storageBucket: 'looks-f90da.appspot.com',
+  messagingSenderId: '337497640410',
+  appId: '1:337497640410:web:29b3486646ff5d644bc4e3',
+  measurementId: 'G-RFV1KWFJ7C',
 };
 
 const paidFirebaseConfig = {
-    apiKey: "AIzaSyCqq4kKHklEWWhKAfUW6mTX0S1F9pKvWD8",
-    authDomain: "looks-5f516.firebaseapp.com",
-    databaseURL: "https://looks-5f516.firebaseio.com",
-    projectId: "looks-5f516",
-    storageBucket: "looks-5f516.appspot.com",
-    messagingSenderId: "556161865767",
-    appId: "1:556161865767:web:c01c02186985c71bd4d602",
-    measurementId: "G-XNX116PFTK"
+  apiKey: 'AIzaSyCqq4kKHklEWWhKAfUW6mTX0S1F9pKvWD8',
+  authDomain: 'looks-5f516.firebaseapp.com',
+  databaseURL: 'https://looks-5f516.firebaseio.com',
+  projectId: 'looks-5f516',
+  storageBucket: 'looks-5f516.appspot.com',
+  messagingSenderId: '556161865767',
+  appId: '1:556161865767:web:c01c02186985c71bd4d602',
+  measurementId: 'G-XNX116PFTK',
 };
 
-const fb = firebase.initializeApp(firebaseConfig);
+const fb = firebase.initializeApp(paidFirebaseConfig);
 export default fb;
