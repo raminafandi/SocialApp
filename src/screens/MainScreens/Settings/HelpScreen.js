6f8b460c0479a941223d733c237e7f49a72ff2ec
Ramin Afandiyev <41924783+raminafandi@users.noreply.github.com>
2020-09-20 21:20:46 +0400

    Algolia implementation beginning

--- src/screens/MainScreens/Settings/HelpScreen.js
+++ src/screens/MainScreens/Settings/HelpScreen.js
@@ -1,32 +1,99 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
-  Image,
-  TouchableOpacity,
-  TextInput,
-  ScrollView,
+  View,
   SafeAreaView,
-  Alert,
+  StatusBar,
+  Button,
 } from 'react-native';
+import {
+  InstantSearch,
+  connectRefinementList,
+} from 'react-instantsearch-native';
+import Filters from './src/Filters';
+import SearchBox from './src/SearchBox';
+import InfiniteHits from './src/InfiniteHits';
+import algoliasearch from 'algoliasearch/lite';
 
-import { window, wsize, hsize } from '../../../entities/constants';
-import { FontAwesome5, MaterialIcons, AntDesign } from '@expo/vector-icons';
-import Option from '../../../components/Option';
-
-const HelpScreen = ({ navigation }) => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={styles.container}></ScrollView>
-    </SafeAreaView>
-  );
-};
+const searchClient = algoliasearch(
+  'RWCGA0GQ1P',
+  '7ff2845ec876110cfa72bf3ea3e0abbd'
+);
 
 const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    backgroundColor: '#252b33',
+  },
   container: {
     flex: 1,
-    paddingTop: hsize(24),
+    backgroundColor: '#FFFFFF',
   },
 });
-export default HelpScreen;
+
+const VirtualRefinementList = connectRefinementList(() => null);
+
+class App extends React.Component {
+  root = {
+    Root: View,
+    props: {
+      style: {
+        flex: 1,
+      },
+    },
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      searchState: {},
+    };
+  }
+
+  toggleModal = () => {
+    this.setState(({ isModalOpen }) => ({
+      isModalOpen: !isModalOpen,
+    }));
+  };
+
+  onSearchStateChange = (searchState) => {
+    this.setState(() => ({
+      searchState,
+    }));
+  };
+
+  render() {
+    const { isModalOpen, searchState } = this.state;
+    return (
+      <SafeAreaView style={styles.safe}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.container}>
+          <InstantSearch
+            searchClient={searchClient}
+            indexName="items"
+            root={this.root}
+            searchState={searchState}
+            onSearchStateChange={this.onSearchStateChange}>
+            <VirtualRefinementList attribute="brand" />
+            <Filters
+              isModalOpen={isModalOpen}
+              searchState={searchState}
+              toggleModal={this.toggleModal}
+              onSearchStateChange={this.onSearchStateChange}
+            />
+            <SearchBox />
+            <Button
+              title="Show Filters Modal"
+              color="red"
+              onPress={this.toggleModal}
+            />
+            <InfiniteHits />
+          </InstantSearch>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
